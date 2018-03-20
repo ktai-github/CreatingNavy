3b81c616c82f4652df21688f9a4a855d7351112f
Kevin <kevin.tai.mail@gmail.com>
2018-03-19 19:55:08 -0700

    sql file runs

--- navy_schema.sql
+++ navy_schema.sql
@@ -1,97 +1,63 @@
+DROP TABLE fleets;
+DROP TABLE ships;
+DROP TABLE duties;
+DROP TABLE sailors;
+DROP TABLE sailorsduties;
 CREATE TABLE fleets (
-fleet-id INTEGER PRIMARY KEY,
-fleet-name VARCHAR(50)
+fleet_id INTEGER PRIMARY KEY AUTOINCREMENT,
+fleet_name CHAR(50)
 );
 CREATE TABLE ships (
-ship-id INTEGER PRIMARY KEY,
-ship-name VARCHAR(50),
-date-built DATE
+ship_id INTEGER PRIMARY KEY AUTOINCREMENT,
+ship_name CHAR(50),
+date_built DATE,
+fleet_id INTEGER
 );
 CREATE TABLE duties (
-duty-id INTEGER PRIMARY KEY,
-duty-name VARCHAR(50),
-ship-id INTEGER,
+duty_id INTEGER PRIMARY KEY AUTOINCREMENT,
+duty_name CHAR(50),
+ship_id INTEGER,
 rank INTEGER,
-start-date DATE,
-end-date DATE
+start_date DATE,
+end_date DATE
 );
 CREATE TABLE sailors (
-sailor-id INTEGER PRIMARY KEY,
-sailor-name VARCHAR(50),
+sailor_id INTEGER PRIMARY KEY AUTOINCREMENT,
+sailor_name CHAR(50),
 dob DATE,
-duty-id INTEGER
+duty_id INTEGER
 );
 CREATE TABLE sailorsduties (
-sailor-id INTEGER,
-duty-id INTEGER
+sailor_id INTEGER,
+duty_id INTEGER
 );
-INSERT INTO fleets (fleet-name)
+INSERT INTO fleets (fleet_name)
 VALUES ('Fleet1');
-INSERT INTO ships (ship-name, date-built)
-VALUES ('Jackdaw');
-INSERT INTO ships (ship-name, date-built)
-VALUES ('Normandy');
-INSERT INTO duties (duty-name, ship-id, rank, start-date, end-date)
-VALUES ('Jackdaw-Captain',
-  SELECT ship-id FROM ships WHERE ship-name = 'Jackdaw',
-  1,
-  '2018-01-01',
-  '2018-01-15'
-);
-INSERT INTO duties (duty-name, ship-id, rank, start-date, end-date)
-VALUES ('Normandy-Captain',
-  SELECT ship-id FROM ships WHERE ship-name = 'Normandy',
-  1,
-  '2018-01-01',
-  '2018-01-15'
-);
-INSERT INTO duties (duty-name, ship-id, rank, start-date, end-date)
-VALUES ('Jackdaw-AssistantCaptain',
-  SELECT ship-id FROM ships WHERE ship-name = 'Jackdaw',
-  2,
-  '2018-01-16',
-  '2018-01-31'
-);
-INSERT INTO duties (duty-name, ship-id, rank, start-date, end-date)
-VALUES ('Normandy-AssistantCaptain',
-  SELECT ship-id FROM ships WHERE ship-name = 'Normandy',
-  2,
-  '2018-01-16',
-  '2018-01-31'
-);
-INSERT INTO sailors (sailor-name, dob, duty-id)
-VALUES ('Edward Kenway',
-  '1693-03-10',
-  SELECT duty-id FROM duties WHERE duty-name = 'Jackdaw-Captain'
-);
-INSERT INTO sailors (sailor-name, dob, duty-id)
-VALUES ('Shepard',
-  '2154-04-11',
-  SELECT duty-id FROM duties WHERE duty-name = 'Normandy-Captain'
-);
-INSERT INTO sailors (sailor-name, dob, duty-id)
-VALUES ('John Smith',
-  '1693-02-10',
-  SELECT duty-id FROM duties WHERE duty-name = 'Jackdaw-AssistantCaptain'
-);
-INSERT INTO sailors (sailor-name, dob, duty-id)
-VALUES ('Jeff Moreau',
-  '2155-01-01',
-  SELECT duty-id FROM duties WHERE duty-name = 'Normandy-AssistantCaptain'
-);
-INSERT INTO sailorsduties (sailor-id, duty-id)
-VALUES (SELECT sailor-id FROM sailors WHERE sailor-name = 'Edward Kenway',
-SELECT duty-id FROM duties WHERE duty-name = 'Jackdaw Captain'
-);
-INSERT INTO sailorsduties (sailor-id, duty-id)
-VALUES (SELECT sailor-id FROM sailors WHERE sailor-name = 'Shepard',
-SELECT duty-id FROM duties WHERE duty-name = 'Normandy Captain'
-);
-INSERT INTO sailorsduties (sailor-id, duty-id)
-VALUES (SELECT sailor-id FROM sailors WHERE sailor-name = 'John Smith',
-SELECT duty-id FROM duties WHERE duty-name = 'Jackdaw AssistantCaptain'
-);
-INSERT INTO sailorsduties (sailor-id, duty-id)
-VALUES (SELECT sailor-id FROM sailors WHERE sailor-name = 'Jeff Moreau',
-SELECT duty-id FROM duties WHERE duty-name = 'Normandy AssistantCaptain'
-);
+INSERT INTO ships (ship_name, date_built, fleet_id)
+VALUES ('Jackdaw', '1600-01-01', 1);
+INSERT INTO ships (ship_name, date_built, fleet_id)
+VALUES ('Normandy', '2100-01-01', 1);
+INSERT INTO duties (duty_name, ship_id, rank, start_date, end_date)
+VALUES ('Jackdaw_Captain', 1, 1, '2018_01_01', '2018_01_15');
+INSERT INTO duties (duty_name, ship_id, rank, start_date, end_date)
+VALUES ('Normandy_Captain', 2, 1, '2018_01_01', '2018_01_15');
+INSERT INTO duties (duty_name, ship_id, rank, start_date, end_date)
+VALUES ('Jackdaw_AssistantCaptain', 1, 2, '2018_01_16', '2018_01_31');
+INSERT INTO duties (duty_name, ship_id, rank, start_date, end_date)
+VALUES ('Normandy_AssistantCaptain', 2, 2, '2018_01_16', '2018_01_31');
+INSERT INTO sailors (sailor_name, dob, duty_id)
+VALUES ('Edward Kenway', '1693_03_10', 1);
+INSERT INTO sailors (sailor_name, dob, duty_id)
+VALUES ('Shepard', '2154_04_11', 2);
+INSERT INTO sailors (sailor_name, dob, duty_id)
+VALUES ('John Smith', '1693_02_10', 3);
+INSERT INTO sailors (sailor_name, dob, duty_id)
+VALUES ('Jeff Moreau', '2155_01_01', 4);
+INSERT INTO sailorsduties (sailor_id, duty_id)
+VALUES (1, 1);
+INSERT INTO sailorsduties (sailor_id, duty_id)
+VALUES (2, 2);
+INSERT INTO sailorsduties (sailor_id, duty_id)
+VALUES (3, 3);
+INSERT INTO sailorsduties (sailor_id, duty_id)
+VALUES (4, 4);
